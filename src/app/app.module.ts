4ef996b37945471f5c35746e99d79bebe2ad894e
FeisalAhmed <feizulf@gmail.com>
2019-12-10 10:36:47 +0300

    imported forms module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
+import { FormsModule } from '@angular/forms';
+
 import { ProfileService } from './services/profile.service';
 
 import { AppRoutingModule } from './app-routing.module';
