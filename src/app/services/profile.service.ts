bcd0e242f41c77ec0a85c235fd44fa8bbfba0bc3
FeisalAhmed <feizulf@gmail.com>
2019-12-10 09:22:41 +0300

    added this.profile

--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -21,4 +21,9 @@ export class ProfileService {
     return this.http.get('https://api.github.com/users/' + this.username + '?client_id' + this.clientid +
     '&client_secret=' + this.clientsecret);
   }
+
+  getProfileRepos() {
+    return this.http.get('https://api.github.com/users/' + this.username + '/repos?client_id' + this.clientid +
+    '&client_secret=' + this.clientsecret);
+  }
 }
